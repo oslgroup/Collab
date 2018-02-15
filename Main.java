ab0e5e57dbf551bdcf7519f984ea1558208c3370
oslgroup <aditya.mankar@somaiya.edu>
2018-02-15 11:27:05 -0800

    Added Selection Sort

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 public class Main
 {
+	public static void SelectionSort(int[] arr)
+	{
+		for (int i = 0; i < arr.length; i++)
+		{
+			int maxIndex = i;
+			for (int j = i + 1; j < arr.length; j++)
+			{
+				if (arr[j] > arr[maxIndex])
+				{
+					maxIndex = j;
+				}
+			}
+			
+			int temp = arr[i];
+			arr[i] = arr[maxIndex];
+			arr[maxIndex] = temp;
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("hello world");
