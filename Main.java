177fcd543499df98d73340e34d44251199f0c456
= <=>
2018-02-16 13:56:53 -0800

    Added Insertion Sort

--- Main.java
+++ Main.java
@@ -19,6 +19,22 @@ public class Main
 		}
 	}
 	
+	public static void InsertionSort(int[] arr)
+	{
+		int j = -1;
+		for (int i = 1; i < n; i++)
+		{
+			int key = arr[i];
+			j = i - 1;
+			while (j >= 0 && arr[j] > key)
+			{
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+	
 	public static void Display(int[] arr)
 	{
 		for (int i = 0; i < arr.length; i++)
